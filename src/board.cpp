0ddfc1420be8752a627e540f2471fd01ef4bc2be
furu_shun1113 <g2227068@st.t-kougei.ac.jp>
2024-04-24 23:27:53 +0900

    だんだん大きく動く経路探索までやった

--- src/board.cpp
+++ src/board.cpp
@@ -16,40 +16,83 @@ std::map<Mass::status, MassInfo> Mass::statusData =
 	{ INVALID,  {-1.0f, '\0'}},
 };
 
-std::mt19937 engine;
+
 bool Board::find(const Point& 始点, const Point& 終点, std::vector<std::vector<Mass>> &mass) const
 {
 	mass[始点.y][始点.x].set(Mass::START);
 	mass[終点.y][終点.x].set(Mass::GOAL);
 
+	std::mt19937 engine;
+
 	// 経路探索
 	Point 現在 = 始点;
-	while (現在 != 終点) {
+	int steps = 0;
+	//とりあえずあり得ない場所で初期化	
+	Point 停止 = 終点;	
+	while (現在 != 終点) 
+	{
 		// 歩いた場所に印をつける(見やすさのために始点は書き換えない)
 		if (現在 != 始点){mass[現在.y][現在.x].set(Mass::WAYPOINT);}
 		
 		// 終点に向かって歩く
 		int dx = 終点.x - 現在.x;
 		int dy = 終点.y - 現在.y;
+		
 		Point 左右 = 現在; 左右.x += (dx > 0) - (dx < 0);
 		Point 上下 = 現在; 上下.y += (dy > 0) - (dy < 0);
 		
 		if (dx * dx < dy * dy)
-		{
+		{			
 			// Y軸方向に近づこうとしてだめならX軸方向に動く
-			if (map_[上下.y][上下.x].canMove()) { 現在 = 上下; continue;}
-			if (map_[左右.y][左右.x].canMove()) { 現在 = 左右; continue;}
+			if (map_[上下.y][上下.x].canMove())
+			{ 
+				現在 = 上下;
+				continue;
+			}
+			if (map_[左右.y][左右.x].canMove()) 
+			{ 
+				現在 = 左右;
+				continue;
+			}
 		}
 		else
 		{
 			//X軸方向に近づこうとしてだめならY軸方向に動く
-			if (map_[左右.y][左右.x].canMove()) { 現在 = 左右; continue;}
-			if (map_[上下.y][上下.x].canMove()) { 現在 = 上下; continue;}
+			if (map_[左右.y][左右.x].canMove()) 
+			{ 
+				現在 = 左右;
+				continue;				
+			}
+			if (map_[上下.y][上下.x].canMove()) 
+			{ 
+				現在 = 上下;
+				continue;
+			}
 		}
-		//動けなくなった場合
-		//４方向の乱数
-		int r = engine() % 4; 
+		//動けなかった場合
+	
+		if (停止 != 現在)
+		{
+			停止 = 現在;
+			steps = 1;
+		}
+		else
+		{
+			steps++;
+		}
+		for (int i = 0; i < steps; i++)
+		{
+			int r = engine() % 4;
+			Point 次 = 現在;
 
+			次.x += (r == 0) - (r == 1);
+			次.y += (r == 2) - (r == 3);
+			if (map_[次.y][次.x].canMove())
+			{
+				現在 = 次;
+				mass[現在.y][現在.x].set(Mass::WAYPOINT);			
+			}
+		}
 	}
 
 	return true;
